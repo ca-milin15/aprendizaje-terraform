5263e4e2befa8ab03f6fff9063d3557e14175a14
PSOFKA0921\camilo.rivera <ccamilorrivera@gmail.com>
2022-09-14 08:30:12 -0500

    Primer commit

---
+++ nerdearla/variables.tf
@@ -0,0 +1,11 @@
+variable "token" {
+  description = "GitHub access token used to configure the provider"
+  type        = string
+  default     = "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"
+}
+
+variable "repositorio" {
+    description = "Creacion de primer variable en el curso Terra"
+    type = string
+    default = "aprendizaje-terraform"
+}
